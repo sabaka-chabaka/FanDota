5403965349d6ea6c136d9ff80f4debbb41a13d2c
sabaka-chabaka <gamekimg360@gmail.com>
2026-03-14 21:17:20 +0400

    Initial commit

---
+++ Source/FanDota/FanDota.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FanDota.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FanDota, "FanDota" );
+
+DEFINE_LOG_CATEGORY(LogFanDota)
+ 
